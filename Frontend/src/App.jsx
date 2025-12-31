f5ec4f59d186c624c770b6c0e76a5d12598da22d
SwapnilSalunke1625 <salunkeswapnil145@gmail.com>
2025-12-31 17:06:40 +0530

    instanceDB connection issue is resolved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { db } from "./store/instancedb";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isLoading } = db.useQuery({
+    posts: {},
+  });
+
+  if (isLoading) return <p>Connecting...</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>InstantDB Connected ✅</h1>
+
+      <button
+      className="bg-black text-white p-3"
+        onClick={() => {
+    console.log("BUTTON CLICKED");
+    db.transact(
+      db.tx.posts.create({
+        title: "Hello InstantDB 🚀",
+        content: "My first post stored in InstantDB",
+        createdAt: Date.now(),
+      })
+    );
+  }}
+        
+         
+      >
+        Add First Post
+      </button>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/store/instancedb.js
@@ -0,0 +1,5 @@
+import { init } from "@instantdb/react";
+
+export const db = init({
+  appId: "efabad7f-d706-4f96-b7cd-a621fc1482a6",
+});
